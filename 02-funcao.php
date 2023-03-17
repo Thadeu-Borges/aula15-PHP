f205c55daa47de08df4fcdc0276252c583f0ebcf
Thadeu <123416610+Thadeu-Borges@users.noreply.github.com>
2023-03-17 10:32:06 -0300

    PHP

---
+++ 02-funcao.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        include "funcoes.php";// include, require, include_once, require_once para incluir arquivos externos em seu script PHP.
+        echo "<h1>Teste novas funções</h1>";
+        ola();
+        mostraValor(4);
+        echo "<h2>Finalizando Programa...</h2>";
+    ?>
+</div>
+</body>
+</html>
+ 
